dd1ef71a02267ed56ceb9bd3d5549c0b7e597641
Farami <webmaster@farami.de>
2015-01-21 08:30:57 +0100

    Changed to one single form of timer

--- public/javascripts/BrewingTime.js
+++ public/javascripts/BrewingTime.js
@@ -1,21 +1,15 @@
-/// <reference path="../../typings/jquery/jquery.d.ts" />
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
 /// <reference path="../../typings/bootstrap/bootstrap.d.ts" />
 /// <reference path="../../typings/node/node.d.ts" />
 "use strict";
 (function () {
     $(".StartTimer").click(function (e) {
         var brewingTimers = $("#BrewingTimers");
+
         var brewingTime = parseInt($(e.currentTarget).data("time"), 10);
         var name = $("#TimerName").val();
         var timer = new Timer(brewingTime, brewingTimers, name !== "" ? name : null);
         timer.start();
-    });
-    $("#ClearTimerName").click(function () {
-        $("#TimerName").val("");
-    });
-    $("#Tabs a").click(function (e) {
-        e.preventDefault();
-        $(this).tab("show");
     });
 })();
 //# sourceMappingURL=BrewingTime.js.map
