2d2d7a96c9986e2a2cad09f4acb25b384eff179a
Farami <webmaster@farami.de>
2015-02-03 11:38:49 +0100

    Added grunt and tasks for min/bundling css/js

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+﻿module.exports = function (grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        cssmin: {
+            sitecss: {
+                options: {
+                    banner: "/* Minified and bundled css for TeaTime. */"
+                },
+                files: {
+                    'public/stylesheets/site.min.css': [
+                        "public/stylesheets/bootstrap.min.css",
+                        "public/stylesheets/style.css"]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                compress: true
+            },
+            applib: {
+                src: [
+                    "public/javascripts/utils/jquery-1.11.1.min.js",
+                    "public/javascripts/AudioElement.js",
+                    "public/javascripts/BrewingTime.js",
+                    "public/javascripts/Helper.js",
+                    "public/javascripts/Timer.js"
+                ],
+                dest: "public/javascripts/site.min.js"
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["uglify", "cssmin"]);
+};
